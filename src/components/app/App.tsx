cb16255e7d4a699202c016d0e831178e7e9e3bc4
AlexanderStreltsov <alexander.streltsov91@yandex.ru>
2023-05-19 15:26:22 +0300

    refactor: edit project template

---
+++ src/components/app/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import './App.scss';
+
+const App = () => (
+  <Router>
+    <Routes>
+      <Route path="/" element={<h1>IHelp</h1>} />
+    </Routes>
+  </Router>
+);
+
+export default App;
