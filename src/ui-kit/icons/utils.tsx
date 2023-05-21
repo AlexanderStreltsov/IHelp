4c220c22e39b5cbb9daad8244bb79560efe3fc29
Victoria Levine <vkpanchenko@gmail.com>
2023-05-21 11:28:12 +0200

    feat: first 3 icons

---
+++ src/ui-kit/icons/utils.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+type TIconColor =
+  | 'white'
+  | 'dark-blue';
+
+export const getColor = (color: TIconColor) => {
+  switch (color) {
+    case 'white':
+      return '#FBFDFF';
+    case 'dark-blue':
+      return '#2E3192';
+    default:
+      // eslint-disable-next-line no-case-declarations, @typescript-eslint/no-unused-vars
+      const exhaustiveCheck: never = color;
+      return '#2E3192';
+  }
+};
+
+export interface IIconProps {
+  color: TIconColor;
+  size?: '24' | '17';
+  className?: string;
+  onClick?:
+    | (() => void)
+    | ((e: React.MouseEvent<SVGSVGElement, MouseEvent>) => void);
+}
