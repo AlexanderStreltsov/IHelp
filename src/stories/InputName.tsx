c7b087609eed44054e20a1d01d5ee7e6cbf2d0d5
Elana Tollu <elana.tollu@yandex.ru>
2023-05-22 14:41:20 +0300

    add InputName component

--- src/stories/InputName.tsx
+++ src/stories/InputName.tsx
@@ -9,12 +9,28 @@ interface Props {
   /**
    * Optional change handler
    */
-  onChange?: () => void;
+  onChange: (value: string) => void;
 }
 
 /**
  * Input field for name
  */
 export const InputName: React.FC<Props> = ({ value, onChange }: Props) => {
-  return <p>Имя: {value}</p>;
+  const inputId = `input-name-${Math.random()}`;
+  const onInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    onChange(e.currentTarget.value);
+  };
+
+  return (
+    <div>
+      <label htmlFor={inputId}>Имя</label>
+      <input
+        id={inputId}
+        type="text"
+        placeholder="Введите имя"
+        value={value}
+        onChange={onInputChange}
+      ></input>
+    </div>
+  );
 };
