ed02f28f1d346ce9f01379c88ea6855febffe84e
denis-ttk-1975 <denis.ttk@gmail.com>
2023-05-22 22:45:57 +0300

    color constant added for tsx

---
+++ src/common/variables.tsx
@@ -0,0 +1,10 @@
+export enum ElementColors {
+  primary = '#fbfdff',
+  popup = '#fff',
+  inactive = '#818c99',
+  partsPrimary = '#2e3192',
+  partsSecondary = '#9798c9',
+  partsActivePrimary = '#0071bc',
+  partsActiveSecondary = '#0091c7',
+  partsActiveAdditional = '#00a99d',
+}
