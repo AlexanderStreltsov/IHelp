795a51fa12f06ad1542ae72e37dc927816634b4d
denis-ttk-1975 <denis.ttk@gmail.com>
2023-05-27 21:59:49 +0300

    story for first icon added

---
+++ src/stories/icons.stories.tsx
@@ -0,0 +1,8 @@
+import { ActiveRequestVersion1Icon } from './../ui/icons/active-request-big-version-1-icon';
+
+export default {
+  title: 'UI/Icons',
+  component: ActiveRequestVersion1Icon,
+};
+
+export const Default = () => <ActiveRequestVersion1Icon color="dark-blue" />;
