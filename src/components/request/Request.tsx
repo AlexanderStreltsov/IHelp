5e055d6a6e36ac386d9ea3b311be2f55884d249a
daryamakavchik <darjamakavchik@gmail.com>
2023-05-26 17:42:26 -0700

    fix prettier & add props

--- src/components/request/Request.tsx
+++ src/components/request/Request.tsx
@@ -11,22 +11,6 @@ import { ClockIcon } from '../../ui/icons/clock-icon';
 import { Button } from '../../ui/button/button';
 import { TTask } from '../../types';
 import api from '../../api';
-import { ActiveRequestVersion1Icon } from '../../ui/icons/active-request-big-version-1-icon';
-import { ActiveRequestVersion2Icon } from '../../ui/icons/active-request-big-version-2-icon';
-import { ApproveIcon } from '../../ui/icons/approve-icon';
-import { ArrowIcon } from '../../ui/icons/arrow-icon';
-import { AttachIcon } from '../../ui/icons/attach-icon';
-import { BlogIcon } from '../../ui/icons/blog-icon';
-import { ContactUsIcon } from '../../ui/icons/contact-us-icon';
-import { ContactsIcon } from '../../ui/icons/contacts-icon';
-import { ExecutedRequestIcon } from '../../ui/icons/executed-request-icon';
-import { KeyIcon } from '../../ui/icons/key-icon';
-import { MessageIcon } from '../../ui/icons/message-icon';
-import { PersonIcon } from '../../ui/icons/person-icon';
-import { PopularIcon } from '../../ui/icons/popular-icon';
-import { PrivacyIcon } from '../../ui/icons/privacy-icon';
-import { StatsIcon } from '../../ui/icons/stats-icon';
-import { UnionIcon } from '../../ui/icons/union-icon';
 
 const onButtonClick = (event: any) => {
   if (event.target.innerHTML === 'Читать') {
@@ -38,17 +22,19 @@ const onButtonClick = (event: any) => {
   }
 };
 
-export const Request: FC = () => {
-  const [tasksState, setTasksState] = useState<TTask[]>();
+export const Request: FC = (props) => {
+  //  Для тестирования функциональности компонента можно раскомментировать код ниже и вместо props использовать объект(ы), приходящий(ие) от апи
 
-  useEffect(() => {
-    async function fetchData() {
-      const tasks = await api.getAllTasks();
-      setTasksState(tasks);
-    }
+  // const [tasksState, setTasksState] = useState<TTask[]>();
 
-    fetchData();
-  }, []);
+  // useEffect(() => {
+  //   async function fetchData() {
+  //     const tasks = await api.getAllTasks();
+  //     setTasksState(tasks);
+  //   }
+
+  //   fetchData();
+  // }, []);
 
   const ps = document.querySelectorAll('p');
   const observer = new ResizeObserver((entries) => {
@@ -65,12 +51,12 @@ export const Request: FC = () => {
 
   return (
     <>
-      {tasksState && tasksState[0] && (
+      {props && (
         <div className="request">
           <div className="categorylogo">
             <p className="logotext">категория</p>
           </div>
-          {tasksState[0].completed === false && (
+          {props.recipient.completed === false && (
             <div className="buttons">
               <Button
                 type="quadrilateralExit"
@@ -88,7 +74,7 @@ export const Request: FC = () => {
                   console.log('hello');
                 }}
               />
-              {tasksState[0].volunteer && (
+              {props.volunteer && (
                 <Button
                   type="quadrilateralEdit"
                   disabled={false}
@@ -105,22 +91,18 @@ export const Request: FC = () => {
               <div
                 className="avatar"
                 style={{
-                  backgroundImage: `url(${tasksState![0].recipient.photo})`,
+                  backgroundImage: `url(${props.recipient.photo})`,
                 }}
               />
               <div className="infotext">
-                <p className="text text-medium">
-                  {tasksState![0].recipient.fullname}
-                </p>
-                <p className="teltext text-medium">
-                  {tasksState![0].recipient.phone}
-                </p>
+                <p className="text text-medium">{props.recipient.fullname}</p>
+                <p className="teltext text-medium">{props.recipient.phone}</p>
               </div>
             </div>
             <div className="icons">
               <Button
                 type="circleSmallPhone"
-                disabled={tasksState![0].completed}
+                disabled={props.recipient.completed}
                 children=""
                 onClick={() => {
                   console.log('hello');
@@ -128,7 +110,7 @@ export const Request: FC = () => {
               />
               <Button
                 type="circleSmallEmail"
-                disabled={tasksState![0].completed}
+                disabled={props.recipient.completed}
                 children=""
                 onClick={() => {
                   console.log('hello');
@@ -137,7 +119,7 @@ export const Request: FC = () => {
             </div>
           </div>
           <div className="content">
-            <h1 className="contentheader text-big">{tasksState![0].title}</h1>
+            <h1 className="contentheader text-big">{props.title}</h1>
             <div className="box text-medium" id="conttext">
               <input type="checkbox" id="expanded" />
               <p>
@@ -146,7 +128,7 @@ export const Request: FC = () => {
                 есть желающие помочь в выгуле собаки, то звоните, 89041627779,
                 Елена. Собаку зовут Айка, порода - немецкая овчарка, возраст -
                 полтора года. Собака очень умная, послушная, добрая, спокойная.
-                {/* {tasksState![0].description} */}
+                {/* {props.description} */}
               </p>
               <label
                 htmlFor="expanded"
@@ -160,29 +142,27 @@ export const Request: FC = () => {
             </div>
 
             <div className="requestcount text-small">
-              {/* {tasksState![0].completed &&  */}
+              {/* {props.completed &&  */}
               <BallsIcon color="dark-blue" />
-              <div>{tasksState![0].recipient.scores}</div>
+              <div>{props.recipient.scores}</div>
             </div>
           </div>
           <div className="category">
             <div className="date">
               <CalendarIcon className="dateicon" color="dark-blue" />
               <p className="datetext">
-                {tasksState![0].date.toString().slice(0, 10).replace(/-/g, '.')}
+                {props.date.toString().slice(0, 10).replace(/-/g, '.')}
               </p>
             </div>
             <div className="time">
               <ClockIcon className="timeicon" color="dark-blue" />
-              <p className="timetext">
-                {tasksState![2].date.toString().slice(11, 16)}
-              </p>
+              <p className="timetext">{props.date.toString().slice(11, 16)}</p>
             </div>
             <div className="address">
               <div>
                 <LocationIcon className="addressicon" color="dark-blue" />
               </div>
-              <p className="addresstext">{tasksState![0].address}</p>
+              <p className="addresstext">{props.recipient.address}</p>
             </div>
           </div>
         </div>
--- src/stories/request.stories.ts
+++ src/stories/request.stories.ts
@@ -17,70 +17,74 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const userOne: Story = {
+export const recipientRequest: Story = {
   args: {
     id: 1,
-    title: 'SomeTitle',
+    title: 'Заголовок Заявки',
     category: {
       id: 2,
-      name: '',
+      name: 'Категория',
     },
     date: new Date(),
-    description: 'description',
+    description:
+      'Пожалуйста, погуляйте с моей собакой, я не смогу ее выгуливать с 12.06 по 24.06 потому что уеду на обследование к врачу. Если есть желающие помочь в выгуле собаки, то звоните, 89041627779, Олег. Собаку зовут Айка, порода - немецкая овчарка, возраст - полтора года. Собака очень умная, послушная, добрая, спокойная.',
     completed: true,
     recipient: {
       id: 1,
-      fullname: 'Ivan Ivanovich',
+      fullname: 'Олегов Олег Олегович',
       role: UserRole.Recipient,
-      vk: 'somevk',
-      photo: 'somephoto',
-      phone: '12234434435',
-      address: 'someaddress',
-      coordinates: [22, 22],
-      approved: null,
-      checked: null,
+      vk: 'https://vk.com/id11111111',
+      photo:
+        'https://plus.unsplash.com/premium_photo-1658507022538-357829d3f95d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2340&q=80',
+      phone: '1 (223) 443 4435',
+      address: 'ул. Бассейная, д.20',
+      coordinates: [22.32908767, 22.32908767],
+      approved: true,
+      checked: true,
       keys: null,
       adminStatus: 3,
-      scores: null,
-      completed: null,
+      scores: 10,
+      completed: false,
     },
     volunteer: null,
     address: 'address',
-    coordinates: [22, 22],
+    coordinates: [22.32908767, 22.32908767],
     chatId: null,
   },
 };
 
-export const userTwo: Story = {
+export const volunteerRequest: Story = {
   args: {
     id: 1,
-    title: 'SomeTitle',
+    title: 'Ещё Заголовок Заявки',
     category: {
       id: 2,
-      name: '',
+      name: 'Категория',
     },
     date: new Date(),
-    description: 'description',
+    description:
+      'Пожалуйста, погуляйте с моими собаками, я не смогу их выгуливать с 18.06 по 28.06 потому что уеду на обследование к врачу. Если есть желающие помочь в выгуле собак, то звоните, 89041627779, Игнат. Собак зовут Айка и Лайка, возраст - полтора года. Собаки очень умные, послушные, добрые, спокойные.',
     completed: true,
     recipient: {
       id: 1,
-      fullname: 'Ivan Ivanovich',
-      role: UserRole.Recipient,
-      vk: 'somevk',
-      photo: 'somephoto',
-      phone: '12234434435',
-      address: 'someaddress',
-      coordinates: [22, 22],
-      approved: null,
-      checked: null,
+      fullname: 'Игнатьев Игнат Игнатович',
+      role: UserRole.Volunteer,
+      vk: 'https://vk.com/id2222222',
+      photo:
+        'https://images.unsplash.com/photo-1601931935934-17c3717239ab?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=927&q=80',
+      phone: '1 (223) 443 4435',
+      address: 'ул. Рассеянная, д.30',
+      coordinates: [22.32908767, 22.32908767],
+      approved: true,
+      checked: true,
       keys: null,
       adminStatus: 3,
-      scores: null,
-      completed: null,
+      scores: 5,
+      completed: true,
     },
     volunteer: null,
     address: 'address',
-    coordinates: [22, 22],
+    coordinates: [22.32908767, 22.32908767],
     chatId: null,
   },
 };
