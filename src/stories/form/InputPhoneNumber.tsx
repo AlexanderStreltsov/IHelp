fa239355ab48138e922626340487da496555516a
Elana Tollu <elana.tollu@yandex.ru>
2023-05-27 20:38:01 +0300

    add phone number validation

--- src/stories/form/InputPhoneNumber.tsx
+++ src/stories/form/InputPhoneNumber.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import styles from './input.module.scss';
 
 interface Props {
@@ -25,11 +25,36 @@ export const InputPhoneNumber: React.FC<Props> = ({
   onChange,
 }: Props) => {
   const [value, setValue] = useState(initialValue);
+  const [error, setError] = useState<string | undefined>();
+  useEffect(() => {
+    if (!error) {
+      setError(errorMessage);
+    }
+  }, [error, errorMessage]);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const isNumberValid = (phoneNumber: string) => {
+    if (!phoneNumber.startsWith('8') && !phoneNumber.startsWith('+7')) {
+      return false;
+    }
+    if (phoneNumber.replace(/\D/g, '').length !== 11) {
+      return false;
+    }
+    return true;
+  };
 
   const inputId = `input-name-${Math.random()}`;
   const onInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setValue(e.currentTarget.value);
-    onChange(e.currentTarget.value);
+    const currentValue = e.currentTarget.value;
+    setValue(currentValue);
+    if (!isNumberValid(currentValue)) {
+      setError('Указан некорректный номер');
+      inputRef.current?.setCustomValidity('Указан некорректный номер');
+    } else {
+      setError(undefined);
+      inputRef.current?.setCustomValidity('');
+    }
+    onChange(currentValue);
   };
 
   return (
@@ -39,17 +64,16 @@ export const InputPhoneNumber: React.FC<Props> = ({
       </label>
       <input
         className={`${styles.input} ${styles.inputPhoneNumber} ${
-          errorMessage ? styles.wrongInput : styles.correctInput
+          error ? styles.wrongInput : styles.correctInput
         }`}
         id={inputId}
         type="tel"
         placeholder="+7 (000) 000 00 00"
         value={value}
         onChange={onInputChange}
+        ref={inputRef}
       ></input>
-      {errorMessage && (
-        <div className={styles.errorMessage}>{errorMessage}</div>
-      )}
+      {error && <div className={styles.errorMessage}>{error}</div>}
     </div>
   );
 };
