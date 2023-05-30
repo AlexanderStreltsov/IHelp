b76e28f8eb96b17a005ca3af8a853138c6def759
denis-ttk-1975 <denis.ttk@gmail.com>
2023-05-30 10:29:41 +0300

    tsx и scss for admin card component created

---
+++ src/components/admin-card/Admin-card.tsx
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from 'react';
+import './admin-card.scss';
+
+export const AdminCard = (props: any) => {
+  return (
+    <>
+      <h1>ADMIN-card</h1>
+    </>
+  );
+};
--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,12 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
+import { AdminCard } from './../admin-card/Admin-card';
 import './App.scss';
 
 const App = () => (
   /* basename need to work deploy into gh pages */
   <Router basename={process.env.PUBLIC_URL}>
     <Routes>
-      <Route path="/" element={<h1>IHelp</h1>} />
+      <Route path="/" element={<AdminCard />} />
     </Routes>
   </Router>
 );
