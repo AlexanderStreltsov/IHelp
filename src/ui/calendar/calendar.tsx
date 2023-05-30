e3508f3091802a407bc4275fd1f188c13e20fff3
AlexanderStreltsov <alexander.streltsov91@yandex.ru>
2023-05-30 16:38:27 +0300

    feat: add time picker

--- src/ui/calendar/calendar.tsx
+++ src/ui/calendar/calendar.tsx
@@ -1,29 +1,48 @@
 import { FC, useState } from 'react';
 import DatePicker, { registerLocale } from 'react-datepicker';
+import MaskedTextInput from 'react-text-mask';
 import ru from 'date-fns/locale/ru';
 
 import './calendar.scss';
 
 registerLocale('ru', ru);
 
-interface ICalendar {
+export interface ICalendar {
   setDate: (date: Date | null) => void;
   inline?: boolean;
+  isTime?: boolean;
 }
 
-export const Calendar: FC<ICalendar> = ({ setDate, inline = true }) => {
+export const Calendar: FC<ICalendar> = ({
+  setDate,
+  inline = true,
+  isTime = false,
+}) => {
   const [startDate, setStartDate] = useState<Date | null>(new Date());
 
   return (
-    <DatePicker
-      locale="ru"
-      selected={startDate}
-      onChange={(date) => {
-        setStartDate(date);
-        setDate(date);
-      }}
-      dateFormatCalendar="LLLL"
-      inline={inline}
-    />
+    <div className="custom-wrapper">
+      {isTime ? 'Время' : 'Дата'}
+      <DatePicker
+        locale="ru"
+        selected={startDate}
+        onChange={(date) => {
+          setStartDate(date);
+          setDate(date);
+        }}
+        dateFormatCalendar="LLLL"
+        dateFormat={isTime ? 'HH:mm' : 'd MMMM yyyy'}
+        customInput={
+          isTime ? (
+            <MaskedTextInput type="text" mask={[/\d/, /\d/, ':', /\d/, /\d/]} />
+          ) : null
+        }
+        showTimeSelect={isTime}
+        showTimeSelectOnly={isTime}
+        timeIntervals={5}
+        timeCaption=""
+        inline={inline}
+      />
+    </div>
   );
 };
