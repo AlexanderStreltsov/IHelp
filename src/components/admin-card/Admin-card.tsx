321a9063020dafaeb312e9f5f29b949543bfc6dc
denis-ttk-1975 <denis.ttk@gmail.com>
2023-05-30 19:57:35 +0300

    scss attached

--- src/components/admin-card/Admin-card.tsx
+++ src/components/admin-card/Admin-card.tsx
@@ -1,10 +1,27 @@
 import React, { useState, useEffect } from 'react';
-import './admin-card.scss';
+import styles from './admin-card.module.scss';
 
-export const AdminCard = (props: any) => {
+type TAdminCardProps = {
+  name: string;
+  surname: string;
+  patronymic: string;
+  personalID: number;
+  tel: string;
+  rights: {
+    verify_accounts: boolean;
+    create_request: boolean;
+    allot_key: boolean;
+    settle_dispute: boolean;
+  };
+};
+
+export const AdminCard = (props: TAdminCardProps) => {
+  const [isInfoOpen, setInfoOpen] = useState(false);
   return (
-    <>
-      <h1>ADMIN-card</h1>
-    </>
+    <div className={`${styles.adminCard}`}>
+      adminCard
+      <div className={`${styles.adminPhoto} `}>adminPhoto</div>
+      <div className={`${styles.adminInfo}`}>adminInfo</div>
+    </div>
   );
 };
--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,13 +1,27 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import { AdminCard } from './../admin-card/Admin-card';
+import { AdminCard } from '../admin-card/Admin-Card';
 import './App.scss';
+
+const stab = {
+  name: 'Иван',
+  surname: 'Иван',
+  patronymic: 'Иван',
+  personalID: 111111111,
+  tel: 'Иван',
+  rights: {
+    verify_accounts: true,
+    create_request: true,
+    allot_key: false,
+    settle_dispute: false,
+  },
+};
 
 const App = () => (
   /* basename need to work deploy into gh pages */
   <Router basename={process.env.PUBLIC_URL}>
     <Routes>
-      <Route path="/" element={<AdminCard />} />
+      <Route path="/" element={<AdminCard {...stab} />} />
     </Routes>
   </Router>
 );
