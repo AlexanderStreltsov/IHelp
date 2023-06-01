4c2fe7fc0eab29b5d3818e483481c46282d9eee0
daryamakavchik <darjamakavchik@gmail.com>
2023-05-31 19:11:21 -0700

    smallfix

--- src/ui/icons/key-icon.tsx
+++ src/ui/icons/key-icon.tsx
@@ -28,7 +28,7 @@ export const KeyIcon = ({ color, ...props }: IIconProps) => (
         filterUnits="userSpaceOnUse"
         colorInterpolationFilters="sRGB"
       >
-        <feFlood flood-opacity="0" result="BackgroundImageFix" />
+        <feFlood floodOpacity="0" result="BackgroundImageFix" />
         <feColorMatrix
           in="SourceAlpha"
           type="matrix"
