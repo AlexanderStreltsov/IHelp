705455c80527d30e01e9b139dc2750277b4421a1
BobrikAU <kirbob0@yandex.ru>
2023-06-01 23:59:41 +0300

    ui-kit: make new select

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,12 +1,25 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
 import './App.scss';
+import { Select } from '../../ui/forms/Select';
 
 const App = () => (
   /* basename need to work deploy into gh pages */
   <Router basename={process.env.PUBLIC_URL}>
     <Routes>
-      <Route path="/" element={<h1>IHelp</h1>} />
+      <Route
+        path="/"
+        element={
+          <>
+            <h1>IHelp</h1>
+            <Select
+              name="city"
+              width="short"
+              initialOptionName="Выберите город"
+              selectableValues={['Камышин', 'Волгоград', 'Санкт-Петербург']}
+            />
+          </>
+        }
+      />
     </Routes>
   </Router>
 );
