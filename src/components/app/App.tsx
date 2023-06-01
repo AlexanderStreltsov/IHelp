2a7e58fd2c72d658d0808fc39b8247e208bf0160
BobrikAU <kirbob0@yandex.ru>
2023-06-02 00:22:50 +0300

    ui: fixed a bug

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.scss';
-import { Select } from '../../ui/forms/Select';
 
 const App = () => (
   /* basename need to work deploy into gh pages */
@@ -11,12 +10,6 @@ const App = () => (
         element={
           <>
             <h1>IHelp</h1>
-            <Select
-              name="city"
-              width="short"
-              initialOptionName="Выберите город"
-              selectableValues={['Камышин', 'Волгоград', 'Санкт-Петербург']}
-            />
           </>
         }
       />
