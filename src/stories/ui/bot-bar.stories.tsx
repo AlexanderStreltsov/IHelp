94652717b7e2b5c89dda4fa8dd9fcac267fc0d58
Victoria Levine <vkpanchenko@gmail.com>
2023-06-03 17:16:28 +0200

    add footer

---
+++ src/stories/ui/bot-bar.stories.tsx
@@ -0,0 +1,8 @@
+import Footer from '../../ui/bot-bar/bot-bar';
+
+export default {
+  title: 'Modules/Header',
+  component: Footer,
+};
+
+export const Default = () => <Footer />;
---
+++ src/ui/bot-bar/bot-bar.tsx
@@ -0,0 +1,8 @@
+import { FC } from 'react';
+import styles from './logo.module.scss';
+
+const Footer: FC = () => {
+  return <footer className={styles.footer} />;
+};
+
+export default Footer;
