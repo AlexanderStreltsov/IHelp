5cb5bfd54050727641a4008c6dba864e1d300445
daryamakavchik <darjamakavchik@gmail.com>
2023-06-02 17:51:25 -0700

    smallfix

--- src/components/page/Page.tsx
+++ src/components/page/Page.tsx
@@ -20,9 +20,9 @@ export const Page = () => {
       {tasksState && tasksState[0] && (
         <>
           <Request propsForRequest={tasksState[0]} owner="admin" />
-          {/* <Request propsForRequest={tasksState[3]} owner="admin" />
+          <Request propsForRequest={tasksState[3]} owner="admin" />
           <Request propsForRequest={tasksState[4]} owner="admin" />
-          <Request propsForRequest={tasksState[15]} owner="admin" /> */}
+          <Request propsForRequest={tasksState[15]} owner="admin" />
         </>
       )}
     </div>
