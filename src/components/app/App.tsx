45e3c7ad7772337dff3025b38f8408f287e0e527
BobrikAU <kirbob0@yandex.ru>
2023-06-04 21:37:47 +0300

    component: clean App.tsx

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,37 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { useState } from 'react';
 
 import './App.scss';
-import { Filter } from '../filters/filter';
 
 const App = () => {
-  const [filter, setFilter] = useState<{ [name: string]: string[] }>({});
-  const [isShowFilter, setIsShowFilter] = useState(true);
-  const getResult = (result: { [name: string]: string[] }) => {
-    setFilter({ ...result });
-    setIsShowFilter(false);
-  };
-  console.log(filter);
   return (
     /* basename need to work deploy into gh pages */
     <Router basename={process.env.PUBLIC_URL}>
       <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              <button onClick={() => setIsShowFilter(true)}>Drück mich</button>
-              <h1>IHelp</h1>
-              {isShowFilter && (
-                <Filter
-                  sendResult={getResult}
-                  currentConditions={filter}
-                  type="volunteerApplicationMap"
-                />
-              )}
-            </>
-          }
-        />
+        <Route path="/" element={<h1>IHelp</h1>} />
       </Routes>
     </Router>
   );
