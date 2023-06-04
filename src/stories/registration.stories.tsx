c33d91c97b7e4a55538b044d152a96a7be8f7e46
vierim <web77@yandex.ru>
2023-06-04 22:13:13 +0300

    story: Add story for Registration component

---
+++ src/stories/registration.stories.tsx
@@ -0,0 +1,8 @@
+import { Registration } from '../components/registration';
+
+export default {
+  title: 'Components/Registration',
+  component: Registration,
+};
+
+export const Default = () => <Registration />;
