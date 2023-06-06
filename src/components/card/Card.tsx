978986f4d14b8684ae059396f0895194afe4cf68
daryamakavchik <darjamakavchik@gmail.com>
2023-06-06 08:49:12 -0700

    fix after review

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,8 +1,4 @@
-/* eslint-disable prefer-const */
-/* eslint-disable @typescript-eslint/no-explicit-any */
-/* eslint-disable @typescript-eslint/no-non-null-assertion */
-/* eslint-disable jsx-a11y/alt-text */
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, SyntheticEvent, FC } from 'react';
 import styles from './card.module.scss';
 import { BallsIcon } from '../../ui/icons/balls-icon';
 import { Button } from '../../ui/button/button';
@@ -10,60 +6,53 @@ import { TUser } from '../../types';
 import figuregreen from '../../images/figuregreen.svg';
 import figuregrey from '../../images/figuregrey.svg';
 import figureorange from '../../images/figureorange.svg';
-import textlabel from '../../images/texthalf.svg';
 import { KeyIcon } from '../../ui/icons/key-icon';
 import { FinishedApplicationIcon } from '../../ui/icons/finished-application-icon';
 
-const onButtonClick = (event: any) => {
+const onButtonClick = (event: SyntheticEvent) => {
   console.log('hello');
 };
 
-export const VolunteerCard = (props: { propsForCard: TUser }) => {
+export const VolunteerCard: FC<TUser> = (props) => {
   const [volunteerState, setVolunteerState] = useState('');
   useEffect(() => {
-    props.propsForCard.checked === true && props.propsForCard.approved === true
+    props.checked === true && props.approved === true
       ? setVolunteerState('approved')
-      : props.propsForCard.approved === true &&
-        props.propsForCard.checked === false
+      : props.approved === true && props.checked === false
       ? setVolunteerState('checked')
       : setVolunteerState('notapproved');
-  }, [props.propsForCard.approved, props.propsForCard.checked]);
+  }, [props.approved, props.checked]);
 
-  const isVolunteer = props.propsForCard.role === 'volunteer';
+  const isVolunteer = props.role === 'volunteer';
 
   return (
-    <>
-      <div className={styles.card}>
-        <div className={styles.cardcontent}>
-          <div
-            className={styles.avatar}
-            style={{ backgroundImage: `url(${props.propsForCard.photo})` }}
-          ></div>
-          <div
-            className={styles.avatarunderlay}
-            style={{
-              backgroundImage:
-                isVolunteer && volunteerState === 'approved'
-                  ? `url(${figuregreen})`
-                  : isVolunteer && volunteerState === 'checked'
-                  ? `url(${figuregrey})`
-                  : isVolunteer && volunteerState === 'notapproved'
-                  ? `url(${figureorange})`
-                  : `url(${''})`,
-            }}
-          ></div>
-          <p className={styles.name}>{props.propsForCard.fullname}</p>
-          <p className={`${styles.id} text-small`}>
-            ID {props.propsForCard.id}
-          </p>
-          <div className={styles.phone}>
-            <p className={`${styles.phonetext} text-small-bold`}> Тел.:</p>
-            <p className={`${styles.phonenumber} text-small`}>
-              {props.propsForCard.phone}
-            </p>
-          </div>
-          <div
-            className={`
+    <div className={styles.card}>
+      <div className={styles.cardcontent}>
+        <div
+          className={styles.avatar}
+          style={{ backgroundImage: `url(${props.photo})` }}
+        ></div>
+        <div
+          className={styles.avatarunderlay}
+          style={{
+            backgroundImage:
+              isVolunteer && volunteerState === 'approved'
+                ? `url(${figuregreen})`
+                : isVolunteer && volunteerState === 'checked'
+                ? `url(${figuregrey})`
+                : isVolunteer && volunteerState === 'notapproved'
+                ? `url(${figureorange})`
+                : `url(${''})`,
+          }}
+        ></div>
+        <p className={styles.name}>{props.fullname}</p>
+        <p className={`${styles.id} text-small`}>ID {props.id}</p>
+        <div className={styles.phone}>
+          <p className={`${styles.phonetext} text-small-bold`}> Тел.:</p>
+          <p className={`${styles.phonenumber} text-small`}>{props.phone}</p>
+        </div>
+        <div
+          className={`
             ${styles.underlay} 
             ${
               isVolunteer && volunteerState === 'approved'
@@ -74,66 +63,52 @@ export const VolunteerCard = (props: { propsForCard: TUser }) => {
                 ? styles.backorange
                 : ''
             }`}
-          >
-            <div className={styles.underlayitems}>
-              <div className={styles.underlayitem}>
-                <BallsIcon color="dark-blue" />
-                <p className={`${styles.underlaynumber} text-small`}>
-                  {props.propsForCard.scores}
-                </p>
-              </div>
-              <div className={styles.underlayitem}>
-                <KeyIcon color="dark-blue" />
-                <p className={`${styles.underlaynumber} text-small`}>
-                  {props.propsForCard.keys ? 1 : 0}
-                </p>
-              </div>
-              <div className={styles.underlayitem}>
-                <FinishedApplicationIcon color="dark-blue" />
-                <p className={`${styles.underlaynumber} text-small`}>
-                  {props.propsForCard.completed
-                    ? props.propsForCard.completed
-                    : 0}
-                </p>
-              </div>
+        >
+          <div className={styles.underlayitems}>
+            <div className={styles.underlayitem}>
+              <BallsIcon color="dark-blue" />
+              <p className={`${styles.underlaynumber} text-small`}>
+                {props.scores}
+              </p>
+            </div>
+            <div className={styles.underlayitem}>
+              <KeyIcon color="dark-blue" />
+              <p className={`${styles.underlaynumber} text-small`}>
+                {props.keys ? 1 : 0}
+              </p>
+            </div>
+            <div className={styles.underlayitem}>
+              <FinishedApplicationIcon color="dark-blue" />
+              <p className={`${styles.underlaynumber} text-small`}>
+                {props.completed ? props.completed : 0}
+              </p>
             </div>
           </div>
-          <div className={styles.buttons}>
-            <Button
-              className={
-                volunteerState === 'checked'
-                  ? styles.halfconfirm
-                  : styles.confirm
-              }
-              type="block"
-              children={volunteerState === 'checked' ? '' : 'Подтвердить'}
-              disabled={volunteerState === 'approved'}
-              onClick={onButtonClick}
-            />
-            {volunteerState === 'checked' && (
-              <img
-                src={textlabel}
-                className={styles.label}
-                onClick={onButtonClick}
-              />
-            )}
-            <Button
-              className={styles.block}
-              type="block"
-              children={'Заблокировать'}
-              disabled={false}
-              onClick={onButtonClick}
-            />
-            <Button
-              className={styles.givekeys}
-              type="block"
-              children={'Дать ключи'}
-              disabled={volunteerState === 'approved'}
-              onClick={onButtonClick}
-            />
-          </div>
+        </div>
+        <div className={styles.buttons}>
+          <Button
+            className={styles.confirm}
+            type="apply"
+            children={'Подтвердить'}
+            disabled={volunteerState === 'approved'}
+            onClick={onButtonClick}
+          />
+          <Button
+            className={styles.block}
+            type="block"
+            children={'Заблокировать'}
+            disabled={false}
+            onClick={onButtonClick}
+          />
+          <Button
+            type="block"
+            className={styles.givekeys}
+            children={'Дать ключи'}
+            disabled={volunteerState === 'approved'}
+            onClick={onButtonClick}
+          />
         </div>
       </div>
-    </>
+    </div>
   );
 };
--- src/stories/card.stories.tsx
+++ src/stories/card.stories.tsx
@@ -4,12 +4,10 @@ import { VolunteerCard } from '../components/card/Card';
 import { UserRole } from '../types/user-role';
 
 const meta = {
-  title: 'Example/VolunteerCard',
+  title: 'Components/VolunteerCard',
   component: VolunteerCard,
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
   tags: ['autodocs'],
   parameters: {
-    // More on how to position stories at: https://storybook.js.org/docs/react/configure/story-layout
     layout: 'fullscreen',
   },
 } satisfies Meta<typeof VolunteerCard>;
@@ -19,87 +17,79 @@ type Story = StoryObj<typeof meta>;
 
 export const volunteerCardApproved: Story = {
   args: {
-    propsForCard: {
-      id: 1982398,
-      fullname: 'Волонтёров Волонтёр Волонтёрович',
-      role: UserRole.Volunteer,
-      vk: 'https://vk.com/id2222222',
-      photo:
-        'https://plus.unsplash.com/premium_photo-1658507022538-357829d3f95d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2340&q=80',
-      phone: '1 (223) 443 4435',
-      address: 'ул. Звездочётная, д.15',
-      coordinates: [22.32908767, 22.32908767],
-      approved: true,
-      checked: true,
-      keys: true,
-      adminStatus: 2,
-      scores: 10,
-      completed: 2,
-    },
+    id: 1982398,
+    fullname: 'Волонтёров Волонтёр Волонтёрович',
+    role: UserRole.Volunteer,
+    vk: 'https://vk.com/id2222222',
+    photo:
+      'https://plus.unsplash.com/premium_photo-1658507022538-357829d3f95d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2340&q=80',
+    phone: '1 (223) 443 4435',
+    address: 'ул. Звездочётная, д.15',
+    coordinates: [22.32908767, 22.32908767],
+    approved: true,
+    checked: true,
+    keys: true,
+    adminStatus: 2,
+    scores: 10,
+    completed: 2,
   },
 };
 
 export const volunteerCardChecked: Story = {
   args: {
-    propsForCard: {
-      id: 11111544,
-      fullname: 'Волонтёров Волонтёр Волонтёрович',
-      role: UserRole.Volunteer,
-      vk: 'https://vk.com/id2222222',
-      photo:
-        'https://plus.unsplash.com/premium_photo-1658507022538-357829d3f95d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2340&q=80',
-      phone: '1 (223) 443 4435',
-      address: 'ул. Звездочётная, д.15',
-      coordinates: [22.32908767, 22.32908767],
-      approved: true,
-      checked: false,
-      keys: false,
-      adminStatus: 2,
-      scores: 10,
-      completed: 2,
-    },
+    id: 11111544,
+    fullname: 'Волонтёров Волонтёр Волонтёрович',
+    role: UserRole.Volunteer,
+    vk: 'https://vk.com/id2222222',
+    photo:
+      'https://plus.unsplash.com/premium_photo-1658507022538-357829d3f95d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2340&q=80',
+    phone: '1 (223) 443 4435',
+    address: 'ул. Звездочётная, д.15',
+    coordinates: [22.32908767, 22.32908767],
+    approved: true,
+    checked: false,
+    keys: false,
+    adminStatus: 2,
+    scores: 10,
+    completed: 2,
   },
 };
 export const volunteerCardNotApproved: Story = {
   args: {
-    propsForCard: {
-      id: 1413144,
-      fullname: 'Волонтёров Волонтёр Волонтёрович',
-      role: UserRole.Volunteer,
-      vk: 'https://vk.com/id2222222',
-      photo:
-        'https://plus.unsplash.com/premium_photo-1658507022538-357829d3f95d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2340&q=80',
-      phone: '1 (223) 443 4435',
-      address: 'ул. Звездочётная, д.15',
-      coordinates: [22.32908767, 22.32908767],
-      approved: false,
-      checked: false,
-      keys: true,
-      adminStatus: 2,
-      scores: 10,
-      completed: 2,
-    },
+    id: 1413144,
+    fullname: 'Волонтёров Волонтёр Волонтёрович',
+    role: UserRole.Volunteer,
+    vk: 'https://vk.com/id2222222',
+    photo:
+      'https://plus.unsplash.com/premium_photo-1658507022538-357829d3f95d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2340&q=80',
+    phone: '1 (223) 443 4435',
+    address: 'ул. Звездочётная, д.15',
+    coordinates: [22.32908767, 22.32908767],
+    approved: false,
+    checked: false,
+    keys: true,
+    adminStatus: 2,
+    scores: 10,
+    completed: 2,
   },
 };
 
 export const recipientCard: Story = {
   args: {
-    propsForCard: {
-      id: 1,
-      fullname: 'Реципиентов Реципиент Реципиентович',
-      role: UserRole.Recipient,
-      vk: 'https://vk.com/id11111111',
-      photo:
-        'https://images.unsplash.com/photo-1601931935934-17c3717239ab?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=927&q=80',
-      phone: '1 (223) 443 4435',
-      address: 'ул. Луночётная, д.15',
-      coordinates: [22.32908767, 22.32908767],
-      approved: true,
-      checked: true,
-      keys: false,
-      adminStatus: 1,
-      scores: 15,
-      completed: 1,
-    },
+    id: 1,
+    fullname: 'Реципиентов Реципиент Реципиентович',
+    role: UserRole.Recipient,
+    vk: 'https://vk.com/id11111111',
+    photo:
+      'https://images.unsplash.com/photo-1601931935934-17c3717239ab?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=927&q=80',
+    phone: '1 (223) 443 4435',
+    address: 'ул. Луночётная, д.15',
+    coordinates: [22.32908767, 22.32908767],
+    approved: true,
+    checked: true,
+    keys: false,
+    adminStatus: 1,
+    scores: 15,
+    completed: 1,
   },
 };
