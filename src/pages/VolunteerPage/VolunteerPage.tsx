ed4a1903113fec68e09c4a33404913032827281c
vierim <web77@yandex.ru>
2023-06-06 22:22:24 +0300

    pages: Add two blocks to Volunteer profile page

--- src/pages/VolunteerPage/VolunteerPage.tsx
+++ src/pages/VolunteerPage/VolunteerPage.tsx
@@ -1,9 +1,32 @@
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
+
+import { Profile } from '../../components/profile/profile';
+
+import { getUser } from '../../api';
+import { TitleBar } from '../../components/title-bar';
+import { ActiveRequestVersion1Icon } from '../../ui/icons/active-request-big-version-1-icon';
 
 const VolunteerPage: FC = () => {
+  const [state, setState] = useState<any>();
+
+  useEffect(() => {
+    const getData = async () => {
+      const volunteer = await getUser(7);
+      setState(volunteer);
+    };
+
+    getData();
+  }, []);
+
   return (
-    <div>
-      <h1 className="text-big">VolunteerPage template</h1>
+    <div className="page-container">
+      <Profile type="volunteer" {...state} />
+      <div className="page-content">
+        <TitleBar
+          title="Активные заявки"
+          icon={<ActiveRequestVersion1Icon color="dark-blue" />}
+        />
+      </div>
     </div>
   );
 };
