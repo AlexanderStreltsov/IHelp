0c0ff67f58eac39f3f38a1f6bbe37ba42788f3e9
vierim <web77@yandex.ru>
2023-06-06 21:50:38 +0300

    feat: Make basic routing and pages with layout

---
+++ src/pages/VolunteerPage/VolunteerPage.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+
+const VolunteerPage: FC = () => {
+  return (
+    <div>
+      <h1 className="text-big">VolunteerPage template</h1>
+    </div>
+  );
+};
+
+export default VolunteerPage;
