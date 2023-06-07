6f025d6733b105f28a2392bc8656eb7a5e7d14fb
Vladimir Ryabtsev <86350622+vierim@users.noreply.github.com>
2023-06-07 22:31:39 +0300

    Add admin cards to chief admin page

--- src/pages/ChiefPage/ChiefPage.tsx
+++ src/pages/ChiefPage/ChiefPage.tsx
@@ -2,16 +2,20 @@ import { FC, useEffect, useState } from 'react';
 
 import { Profile } from '../../components/profile/profile';
 import { TitleBar } from '../../components/title-bar';
+import { Filter } from '../../components/filters/filter';
 import { ConfirmIcon } from '../../ui/icons/confirm-icon';
 
 import { getAllUsers } from '../../api';
 
 import type { TUser } from '../../types';
+import { AdminCard } from '../../components/admin-card/Admin-card';
 
 const ChiefPage: FC = () => {
   const [profile, setProfile] = useState<TUser>();
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [adminsList, setAdminsList] = useState<TUser[]>();
+
+  const [filter, setFilter] = useState({});
+  const [isShowFilter, setIsShowFilter] = useState(false);
 
   useEffect(() => {
     const getData = async () => {
@@ -27,6 +31,15 @@ const ChiefPage: FC = () => {
     getData();
   }, []);
 
+  const filterToggle = () => {
+    setIsShowFilter((prev) => !prev);
+  };
+
+  const getResult = (result: { [name: string]: string[] }) => {
+    setFilter({ ...result });
+    setIsShowFilter(false);
+  };
+
   return (
     <div className="page-container">
       <Profile type="volunteer" {...profile} />
@@ -34,7 +47,36 @@ const ChiefPage: FC = () => {
         <TitleBar
           title="Подтверждение / Блокировка"
           icon={<ConfirmIcon color="dark-blue" />}
+          filterHandler={filterToggle}
         />
+        {isShowFilter && (
+          <Filter
+            sendResult={getResult}
+            currentConditions={filter}
+            type="admin"
+          />
+        )}
+        <div className="catalog">
+          {adminsList &&
+            adminsList?.map((item) => {
+              const rightConfig = {
+                verify_accounts: !!(item.adminStatus && item.adminStatus > 0),
+                create_request: !!(item.adminStatus && item.adminStatus > 1),
+                allot_key: !!(item.adminStatus && item.adminStatus > 2),
+                settle_dispute: !!(item.adminStatus && item.adminStatus > 3),
+              };
+
+              return (
+                <AdminCard
+                  photo={item.photo}
+                  fullName={item.fullname || ''}
+                  personalID={item.id || 0}
+                  tel={item.phone || ''}
+                  rights={rightConfig}
+                />
+              );
+            })}
+        </div>
       </div>
     </div>
   );
