838a7215cac8b2aff4846e4cd368671a9ec60401
Victoria Levine <vkpanchenko@gmail.com>
2023-06-07 14:35:56 +0200

    Revert "fix prettier"
    This reverts commit ac667c8b0e720c8ef47987494b58d4739a39637e.

--- src/stories/recipient-card.stories.tsx
+++ src/stories/recipient-card.stories.tsx
@@ -1,7 +1,7 @@
 import { RecipientCard } from '../components/recipient-card/RecipientCard';
 import type { Meta, StoryObj } from '@storybook/react';
 
-const meta = {
+const meta =  {
   title: 'Components/RecipientCard',
   component: RecipientCard,
   tags: ['autodocs'],
@@ -14,11 +14,11 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const recipientCard: Story = {
-  args: {
-    id: 11111114,
-    fullname: 'Петров Петр Петрович',
-    photo:
-      'https://github.githubassets.com/images/modules/open_graph/github-octocat.png',
-    phone: '+7 (000) 000-00-04',
-  },
-};
+    args: {
+      id: 11111114,
+      fullname: 'Петров Петр Петрович',
+      photo:
+        'https://images.unsplash.com/photo-1601931935934-17c3717239ab?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=927&q=80',
+      phone: '+7 (000) 000-00-04'
+    },
+  };
