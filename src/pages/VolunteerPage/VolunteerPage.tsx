552eff13582f084687c4be73f7e783f47138395a
Vladimir Ryabtsev <86350622+vierim@users.noreply.github.com>
2023-06-08 01:26:41 +0300

    Add Request to volunteer page

--- src/pages/VolunteerPage/VolunteerPage.tsx
+++ src/pages/VolunteerPage/VolunteerPage.tsx
@@ -5,18 +5,21 @@ import { Profile } from '../../components/profile/profile';
 import { Sidebar } from '../../components/sidebar';
 import { TitleBar } from '../../components/title-bar';
 import { Filter } from '../../components/filters/filter';
+import { Request } from '../../components/request/Request';
 
 import { Button } from '../../ui/button/button';
 import { ActiveRequestVersion1Icon } from '../../ui/icons/active-request-big-version-1-icon';
 
-import { getAllUsers } from '../../api';
+import api from '../../api';
 
-import type { TUser } from '../../types';
+import type { TTask, TUser } from '../../types';
 
 import styles from './VolunteerPage.module.scss';
 
 const VolunteerPage: FC = () => {
   const [profile, setProfile] = useState<TUser>();
+  const [tasksState, setTasksState] = useState<TTask[]>();
+
   const [filter, setFilter] = useState({});
   const [isShowFilter, setIsShowFilter] = useState(false);
 
@@ -24,8 +27,12 @@ const VolunteerPage: FC = () => {
 
   useEffect(() => {
     const getData = async () => {
-      const data = (await getAllUsers()).filter((user) => user.id === 7);
+      const data = (await api.getAllUsers()).filter((user) => user.id === 7);
+      const tasks = (await api.getAllTasks()).filter(
+        (task) => task.volunteer?.id === 7,
+      );
 
+      setTasksState(tasks);
       setProfile(data[0]);
     };
 
@@ -73,6 +80,12 @@ const VolunteerPage: FC = () => {
           icon={<ActiveRequestVersion1Icon color="dark-blue" />}
           filterHandler={filterToggle}
         />
+        <div className="catalog catalog-column">
+          {tasksState &&
+            tasksState?.map((item) => {
+              return <Request propsForRequest={item} owner="admin" />;
+            })}
+        </div>
       </div>
       {isShowFilter && (
         <Filter
