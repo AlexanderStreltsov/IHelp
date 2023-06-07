07e5f88da1004f028569480c2ee8fa5b2d9829ea
web77code <web77@yandex.ru>
2023-06-07 12:21:16 +0300

    feat: Add filter to VolunteerPage

--- src/pages/VolunteerPage/VolunteerPage.tsx
+++ src/pages/VolunteerPage/VolunteerPage.tsx
@@ -2,6 +2,7 @@ import { FC, useEffect, useState } from 'react';
 
 import { Profile } from '../../components/profile/profile';
 import { TitleBar } from '../../components/title-bar';
+import { Filter } from '../../components/filters/filter';
 import { ActiveRequestVersion1Icon } from '../../ui/icons/active-request-big-version-1-icon';
 
 import { getUser } from '../../api';
@@ -10,6 +11,8 @@ import type { TUser } from '../../types';
 
 const VolunteerPage: FC = () => {
   const [profile, setProfile] = useState<TUser>();
+  const [filter, setFilter] = useState({});
+  const [isShowFilter, setIsShowFilter] = useState(false);
 
   useEffect(() => {
     const getData = async () => {
@@ -21,6 +24,15 @@ const VolunteerPage: FC = () => {
     getData();
   }, []);
 
+  const filterToggle = () => {
+    setIsShowFilter((prev) => !prev);
+  };
+
+  const getResult = (result: { [name: string]: string[] }) => {
+    setFilter({ ...result });
+    setIsShowFilter(false);
+  };
+
   return (
     <div className="page-container">
       <Profile type="volunteer" {...profile} />
@@ -28,8 +40,16 @@ const VolunteerPage: FC = () => {
         <TitleBar
           title="Активные заявки"
           icon={<ActiveRequestVersion1Icon color="dark-blue" />}
+          filterHandler={filterToggle}
         />
       </div>
+      {isShowFilter && (
+        <Filter
+          sendResult={getResult}
+          currentConditions={filter}
+          type="volunteerApplicationMap"
+        />
+      )}
     </div>
   );
 };
